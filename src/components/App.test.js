b70ff8e3bb71a0b58031a266ba40025a8af67469
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-19 18:36:09 +0200

    more expects for componentes

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -13,4 +13,18 @@ describe('App', () => {
 	it('renders the clear button', () => {
 		expect(app.find('.btn').at(1).text()).toEqual('Clear Notes');
 	});
+
+	describe('when rendering the form', () => {
+		it('creates a Form component', () => {
+			expect(app.find('Form').exists()).toBe(true);
+		});
+
+		it('renders a FormControl component', () => {
+			expect(app.find('FormControl').exists()).toBe(true);
+		});
+
+		it('renders a submit button', () => {
+			expect(app.find('.btn').at(0).text()).toEqual('Submit');
+		});
+	});
 });
