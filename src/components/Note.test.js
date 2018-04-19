b04c4ac9d23bd00ea0a026af0d30618cb71d0ef7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-19 11:21:15 +0200

    using epect

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Note from './Note';
 
+const props = { note: { text: 'test note'} }
+
 describe('Note', () => {
-	let note = mount(<Note />);
+	let note = mount(<Note {...props} />);
+
+	it('renders the note text', () => {
+		expect(note.find('p').text()).toEqual(props.note.text);
+	});
 });
